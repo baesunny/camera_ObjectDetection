bba03a263df8ec04c236a157d6500aad133bc3a3
aloeeola <aloeeola@yonsei.ac.kr>
2024-04-08 10:07:37 +0900

    [feat] headPose

---
+++ headPose/headPose_wCoord.py
@@ -0,0 +1,74 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import time
+from gtts import gTTS
+import os
+import pygame 
+
+mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+
+mp_drawing = mp.solutions.drawing_utils
+
+drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+
+
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    success, image = cap.read()
+
+    start = time.time()
+
+    # Flip the image horizontally for a later selfie-view display
+    # Also convert the color space from BGR to RGB
+    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+
+    # To improve performance
+    image.flags.writeable = False
+    
+    # Get the result
+    results = face_mesh.process(image)
+    
+    # To improve performance
+    image.flags.writeable = True
+    
+    # Convert the color space from RGB to BGR
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    img_h, img_w, img_c = image.shape
+    face_3d = []
+    face_2d = []
+
+    
+    if results.multi_face_landmarks:
+        for face_landmarks in results.multi_face_landmarks:
+            for idx, lm in enumerate(face_landmarks.landmark):
+                if idx == 33 or idx == 263 or idx == 1 or idx == 61 or idx == 291 or idx == 199:
+                    if idx == 1:
+                        nose_2d = (lm.x * img_w, lm.y * img_h)
+                        nose_3d = (lm.x * img_w, lm.y * img_h, lm.z * 3000)
+
+                    x, y = int(lm.x * img_w), int(lm.y * img_h)
+                    text = f"({x}, {y})" 
+                
+    text_offset_x = 20 # X offset from the face
+    text_offset_y = 20 # Y offset from the face
+    cv2.putText(image, text, (x + text_offset_x, y + text_offset_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+
+    end = time.time()
+    totalTime = end - start
+
+    fps = 1 / totalTime
+        #print("FPS: ", fps)
+
+    cv2.putText(image, f'FPS: {int(fps)}', (20,450), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
+
+    cv2.imshow('Head Pose Estimation', image)
+
+    if cv2.waitKey(5) & 0xFF == 27:
+        break
+
+
+cap.release()
