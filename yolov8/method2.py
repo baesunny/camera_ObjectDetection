a03d839ed84ffef40e795884c581761402c1c9c8
ParkSeokwoo <car052000@snu.ac.kr>
2024-05-22 19:46:59 +0900

    0522

--- yolov8/method2.py
+++ yolov8/method2.py
@@ -6,51 +6,16 @@ from ultralytics import YOLO
 from src.utils import intersection_over_union
 from src.elements import face_detect, head_Pose
 import torch
-from torchvision.transforms import functional as F
+from torchvision.transforms import functional as F 
 
-from gtts import gTTS
-import pygame
-import tempfile
+import pyttsx3
 import threading
-import os
 
-class VoiceAssistant:
-    def __init__(self):
-        pygame.mixer.init()
-        self.lock = threading.Lock()
-        self.is_speaking = False
-
-    def speak(self, text):
-        with self.lock:
-            if not self.is_speaking:
-                self.is_speaking = True
-                threading.Thread(target=self._speak_task, args=(text,)).start()
-
-    def _speak_task(self, text):
-        # gTTS를 사용하여 오디오 파일 생성
-        tts = gTTS(text=text, lang='ko')
-        temp_file = tempfile.NamedTemporaryFile(delete=False, suffix='.mp3')
-        tts.save(temp_file.name)
-        temp_file.close()
-        
-        # 오디오 제생
-        pygame.mixer.music.load(temp_file.name)
-        pygame.mixer.music.play()
-        while pygame.mixer.music.get_busy():  # 재생이 끝날 때까지 대기
-            pygame.time.Clock().tick(10)
-        
-        os.remove(temp_file.name)
-        with self.lock:
-            self.is_speaking = False
-
-    def stop_speaking(self):
-        with self.lock:
-            if self.is_speaking:
-                pygame.mixer.music.stop()  # 재생 중지
-                self.is_speaking = False
-
-    
-voice_assistant = VoiceAssistant()
+torch.cuda.empty_cache()
+engine = pyttsx3.init()
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
 
 # Check if CUDA is available and set the device accordingly
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -63,7 +28,7 @@ print(f"Using device: {device}")
 # coco128.close()
 class_list = ['face']
 
-model = YOLO(r'C:\Users\baseoki\OneDrive\바탕 화면\프로젝트\camera_ObjectDetection\custom_dataset 학습 코드 YOLO\runs\detect\에포크 100 patience 10\weights\best.pt')
+model = YOLO(r'C:\Users\baseoki\Downloads\train6.pt')
 model.to(device)  # Use the device set above
 
 mp_face_mesh = mp.solutions.face_mesh
@@ -149,13 +114,13 @@ while True:
             if iou < iou_threshold:
                 last_change_time = time.time()
                 state_loc_variable = 1
-                voice_assistant.stop_speaking()
+ 
                 # voice_assistant.shutdown()
             elif (time.time() - last_change_time) > stable_threshold_time and state_loc_variable == 1:
                 pct, loc = face_detect(xmin, ymin, xmax, ymax, frame)
                 #if pct > pct_threshold:
                 text = f"현재 얼굴 비중은 {pct}%이고 얼굴 위치는 {loc}입니다."
-                voice_assistant.speak(text)
+                threading.Thread(target=speak, args=(text,)).start()
                 last_change_time = time.time()
                 state_loc_variable = 0
 
