29409433c41015409537ca4e098595d63112af15
aloeeola <aloeeola@yonsei.ac.kr>
2024-06-03 04:03:30 +0900

    final version (Albumentations 버전충돌시 src>utils>improve_frame 함수와 rear_mode.py의 line84를 주석처리, line85의 improvedFrame을 frame으로 변경하여 test 바람

--- Real/home/pages/rear_mode.py
+++ Real/home/pages/rear_mode.py
@@ -1,188 +1,210 @@
-import cv2
-import torch
-from torchvision.transforms import functional as F
-from ultralytics import YOLO
-from PIL import Image
-
+# utils
 import streamlit as st
-import numpy as np
-import tempfile
 import time
-import mediapipe as mp
+import torch
+import cv2
+from ultralytics import YOLO
 
-import sys
-import os
+# functions
+from pages.src.utils import iou_multiple, improve_frame, showGrid, save_image
+from pages.src.elements import detect, box_to_pct
 
-# Add the 'home' directory to the sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
-from pages.src.utils import intersection_over_union
-from pages.src.elements import detect, head_Pose
-
+# tts
 import pyttsx3
 import threading
+torch.cuda.empty_cache()
 engine = pyttsx3.init()
-
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
 # Check if CUDA is available and set the device accordingly
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(f"Using device: {device}")
 
-# Load the model
-class_list = ['face']
-model = YOLO('C:/Users/happy/Desktop/camera_ObjectDetection/전면카메라/home/best.pt')
-model.to(device)
-mp_face_mesh = mp.solutions.face_mesh
-face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5)
-
-# hyperparameter setting
-CONFIDENCE_THRESHOLD = 0.5    
-iou_threshold = 0.8          
-stable_threshold_time = 1.5   
-pct_threshold = 10   
-
-# constants
-GREEN = (0, 255, 0)       
-WHITE = (255, 255, 255)   
-BLUE = (0, 0, 255)
+# Load pretrained model
+model = YOLO("./weights/yolov8n.pt")
+class_list = model.names
+class_list_korean = [
+    "사람", "자전거", "자동차", "오토바이", "비행기", "버스", "기차", "트럭", "보트", "신호등", "소화전", "정지 신호", "주차 미터기", "벤치", "새", "고양이", "개", "말", "양", "소", "코끼리", "곰", "얼룩말", "기린", "배낭", "우산", "핸드백", "넥타이", "여행가방", "프리스비", "스키", "스노보드", "스포츠 공", "연", "야구 방망이", "야구 글러브", "스케이트보드", "서핑보드", "테니스 라켓", "병", "와인 잔", "컵", "포크", "나이프", "숟가락", "그릇", "바나나", "사과", "샌드위치", "오렌지", "브로콜리", "당근", "핫도그", "피자", "도넛", "케이크", "의자", "소파", "화분", "침대", "식탁", "화장실", "TV", "노트북", "마우스", "리모컨", "키보드", "휴대전화", "전자레인지", "오븐", "토스터", "싱크대", "냉장고", "책", "시계", "꽃병", "가위", "테디 베어", "헤어 드라이어", "칫솔"
+]
 
 
 def main():
-    st.title("후면카메라")
+    st.title("후면카메라:camera:")
     
     # Initialize the state variables
     if 'capture_button_pressed' not in st.session_state:
         st.session_state.capture_button_pressed = False
-    if 'captured_image_path' not in st.session_state:
-        st.session_state.captured_image_path = None
-    if 'stop_button_pressed' not in st.session_state:
-        st.session_state.stop_button_pressed = False
+    if 'image_captured' not in st.session_state:
+        st.session_state.image_captured = False
 
     frame_placeholder = st.empty()
     capture_button = st.button("Capture")
-    stop_button = st.button("Stop")
-
-    last_change_time = time.time()
-    last_bbox = None
-    max_area = 0
-    max_box = None
-    original_dim = (1280, 720)
-    resize_dim = (640, 640)
-    # Scale factors for coordinates
-    x_scale = original_dim[0] / resize_dim[0]
-    y_scale = original_dim[1] / resize_dim[1]
-
-    state_loc_variable = 1
 
     cap = cv2.VideoCapture(0)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+    width, height = 1280, 720
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
-    while cap.isOpened() and not st.session_state.stop_button_pressed:
+    # hyperparameter setting
+    CONFIDENCE_THRESHOLD = 0.6  
+    iou_threshold = 0.8   
+    box_threshold = 10 # FPS 고려    
+    stable_threshold_time = 1.5   
+    change_threshold_time = 0.5
+
+    # constants
+    GREEN = (0, 255, 0)       
+    WHITE = (255, 255, 255)   
+    BLUE = (0, 0, 255)
+
+    last_change_time = time.time()
+    last_stable_time = time.time()
+    last_bboxes = None
+    detections = None
+
+    state_loc_variable = 1
+    num_frame = 0
+    change_variable = 0
+
+    while cap.isOpened():
         start = time.time()
-        print(start)
         success, frame = cap.read()
+        num_frame += 1
+
         if not success:
-            st.write("Camera Error")
+            st.write("Cam Error")
             break
-      
-        # Head pose estimation
-        headpose = head_Pose(image=frame, face_mesh=face_mesh)
+
+        # Left/Right Inversion
         frame = cv2.flip(frame, 1)
-        ##### TODO : 요소3 안내 조건 조정 및 추가 #####
-        cv2.putText(frame, headpose, (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, GREEN, 2)
-        
-        # YOLO
-        frame_resized = cv2.resize(frame, (640, 640))
-        frame_tensor = F.to_tensor(frame_resized).unsqueeze(0).to(device)
-        detection = model.predict(frame_tensor,conf=CONFIDENCE_THRESHOLD)[0].cpu()
 
-        for data in detection.boxes.data.tolist(): 
-            xmin, ymin, xmax, ymax, conf, label = int(data[0]), int(data[1]), int(data[2]), int(data[3]), float(data[4]), int(data[5])
-            xmin = int(xmin * x_scale)
-            ymin = int(ymin * y_scale)
-            xmax = int(xmax * x_scale)
-            ymax = int(ymax * y_scale)
+        # Model Inference
+        improvedFrame = improve_frame(frame)
+        results = model.predict(improvedFrame, conf=CONFIDENCE_THRESHOLD)
 
-            if label!=0:
-                continue
+        # Extract bounding boxes, labels, and scores
+        if results:
+            new_bboxes=[]
+            for data in results:
+                boxes = data.boxes.xyxy.cpu().numpy()
+                confs = data.boxes.conf.cpu().numpy()
+                labels = data.boxes.cls.cpu().numpy()
+                pcts = box_to_pct(boxes, width, height)
+
+            detections = sorted(zip(boxes, confs, labels, pcts), key=lambda x: x[1], reverse=True)
             
-            area = (xmax-xmin)*(ymax-ymin)
-            if area > max_area:
-                max_area = area
-                max_box = [xmin, ymin, xmax, ymax, conf, label]
-        
-        if max_box:
-            xmin, ymin, xmax, ymax, conf, label = max_box
-            new_bbox = [xmin, ymin, xmax, ymax]
-            cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), GREEN, 2)
-            cv2.putText(frame, class_list[label]+' '+str(round(conf, 2)), (xmin, ymin-10), cv2.FONT_ITALIC, 1, WHITE, 2)
+            if len(detections) > 3:
+                detections = detections[:3]
+                boxes = boxes[:3]
 
-            if last_bbox is not None:
-                iou = intersection_over_union(last_bbox, new_bbox)
+        for idx, box in enumerate(boxes):
+            xmin, ymin, xmax, ymax = map(int, box)
+            new_bboxes.append([xmin, ymin, xmax, ymax, labels[idx], pcts[idx]])  
+        
+        # Assume 1 new detected object & TTS
+        if len(detections) == 1:     
+            if last_bboxes is not None:
+                iou = iou_multiple(last_bboxes, new_bboxes)
+                box, conf, label, pct = detections[0]
+                xmin, ymin, xmax, ymax = map(int, box)
+                label_name = class_list[int(label)]
+                cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), GREEN, 2)
+                cv2.putText(frame, f"{label_name} {conf:.2f}", (xmin, ymin - 10), cv2.FONT_ITALIC, 1, WHITE, 2)
+           
                 if iou < iou_threshold:
                     last_change_time = time.time()
-                    state_loc_variable = 1
+                    if change_variable == 0:
+                        change_variable = 1
+                    else:
+                        if last_change_time - last_stable_time > change_threshold_time:
+                            state_loc_variable = 1
+                elif change_variable == 1:
+                    last_stable_time = time.time()
+                    change_variable = 0
+                
+                if (time.time() - last_change_time) > stable_threshold_time and state_loc_variable == 1:
+                    box, conf, label, pct = detections[0]
+                    xmin, ymin, xmax, ymax = map(int, box)
+                    label_name_korean = class_list_korean[int(label)]
+                
+                    pct, loc = detect(xmin, ymin, xmax, ymax, frame)
+                    text = f"현재 {label_name_korean}의 비중은 {pct}%이고 위치는 {loc}입니다."
+                    threading.Thread(target=speak, args=(text,)).start()
+                    last_stable_time = time.time()
+                    state_loc_variable = 0
 
-                elif (time.time() - last_change_time) > stable_threshold_time and state_loc_variable == 1:
-
-                    # 요소 1 & 2 탐지 시작
-                    pct, loc = detect(xmin,ymin,xmax,ymax,frame)
-                    # (요소1)
-                    if pct > pct_threshold: # TODO: 요소1 조건 조정
-                        pct_text = f"object percentage: {pct}%"
-                        cv2.putText(frame, pct_text, (20, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, GREEN, 2)
-                    # (요소2)
-                    loc_text = f"object location : {loc}"
-                    cv2.putText(frame, loc_text, (20, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.5, GREEN, 2)
-                    # (TTS)
-                    threading.Thread(target=speak, args=(f"현재 얼굴 비중은 {pct}%이고 얼굴 위치는 {loc}이다.",)).start()
-
-                    # 안내 완료 후 타이머 리셋
+        # Assume several new detected objects & TTS
+        elif len(detections) > 1:
+            if last_bboxes is not None:
+                iou = iou_multiple(last_bboxes, new_bboxes)
+                for j in detections:
+                    box, conf, label, pct = j
+                    xmin, ymin, xmax, ymax = map(int, box)
+                    label_name = class_list[int(label)]
+                    
+                    cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), GREEN, 2)
+                    cv2.putText(frame, f"{label_name} {conf:.2f}", (xmin, ymin - 10), cv2.FONT_ITALIC, 1, WHITE, 2)
+                
+                if iou < iou_threshold:
                     last_change_time = time.time()
-                    # state_loc_variable 리셋
-                    state_loc_variable = 0 
+                    if change_variable == 0:
+                        change_variable = 1
+                    else:
+                        if last_change_time - last_stable_time > change_threshold_time:
+                            state_loc_variable = 1
+                elif change_variable == 1:
+                    last_stable_time = time.time()
+                    change_variable = 0
+                    
+                if (time.time() - last_change_time) > stable_threshold_time and state_loc_variable == 1:
+                    text="현재"
+                    for j in detections:
+                        box, conf, label, pct = j
+                        xmin, ymin, xmax, ymax = map(int, box)
+                        label_name = class_list[int(label)]
+                        label_name_korean = class_list_korean[int(label)]
+                        
+                        pct, loc = detect(xmin, ymin, xmax, ymax, frame)
+                        text += f" {label_name_korean}의 위치는 {loc}입니다."
+                    
+                    threading.Thread(target=speak, args=(text,)).start()
+                    last_stable_time = time.time()
+                    state_loc_variable = 0
+    
+        if new_bboxes and num_frame % box_threshold == 0:
+            if not (change_variable == 1 and state_loc_variable == 0):
+                last_bboxes = new_bboxes
             
-            last_bbox = new_bbox
-
-        max_area = 0
-        max_box = None
-
-        # fps 계산
+        
+        # fps calculation
         end = time.time()
         totalTime = end - start
         fps = f'FPS: {1 / totalTime:.2f}'
         print(f'Time to process 1 frame: {totalTime * 1000:.0f} milliseconds')
         print(fps)
-        cv2.putText(frame, fps, (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, BLUE, 2) # fps 표시
+        cv2.putText(frame, fps, (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, BLUE, 2)
 
-      
+        # show frame 
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        showGrid(frame)
         frame_placeholder.image(frame, channels = "RGB")
 
-        
-        if capture_button:
-            st.session_state['capture_button_pressed'] = True
-            with tempfile.NamedTemporaryFile(delete=False,suffix='png') as tmp_file:
-                image = Image.fromarray(frame)
-                image.save(tmp_file.name)
-                st.session_state.captured_image_path = tmp_file.name
-        
+        # capture 기능
+        if capture_button and not st.session_state.capture_button_pressed:
+            st.session_state.capture_button_pressed = True
+            save_image(frame=frame)
+
+            st.session_state.image_captured=True
+            capture_button=False
+            st.session_state.capture_button_pressed=False
+
+        if st.session_state.image_captured:
             st.success("Image Captured!")
-            st.session_state['capture_button_pressed'] = False
+            st.session_state.image_captured = False
 
-
-        if st.session_state.captured_image_path:
-            st.markdown(f"[Download captured image](file://{st.session_state.captured_image_path})")
-
-            st.success("Image Downloaded!")
-            st.session_state['captured_image_path'] = None
-
-        if stop_button:
-            st.session_state['stop_button_pressed'] = True
-
-        if cv2.waitKey(1) == ord("q") or st.session_state.stop_button_pressed:
+        if cv2.waitKey(1) == ord("q"):
             break
 
     cap.release()
